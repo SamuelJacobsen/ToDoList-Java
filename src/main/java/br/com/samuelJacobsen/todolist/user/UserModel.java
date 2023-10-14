e0d67994631a58416c1609612b94a9974111f657
Samuel Penha Jacobsen <samuelpj1606@gmail.com>
2023-10-14 12:48:58 -0300

    Salvando dados de usuario

--- src/main/java/br/com/samuelJacobsen/todolist/user/UserModel.java
+++ src/main/java/br/com/samuelJacobsen/todolist/user/UserModel.java
@@ -1,7 +1,30 @@
 package br.com.samuelJacobsen.todolist.user;
 
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+import org.hibernate.annotations.CreationTimestamp;
+
+import jakarta.annotation.Generated;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import lombok.Data;
+
+@Data
+@Entity(name = "tb_users")
 public class UserModel {
+
+    @Id
+    @GeneratedValue(generator = "UUID")
+    private UUID id;
+
+   
     public String username;
     public String name;
     public String password;
+
+    @CreationTimestamp
+    private LocalDateTime createdAt;
+
 }
