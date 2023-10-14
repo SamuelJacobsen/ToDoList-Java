e545f4e0a20712171c582df0e41c002f7c071728
Samuel Penha Jacobsen <samuelpj1606@gmail.com>
2023-10-14 12:57:36 -0300

    Tratando dados de usuario

--- src/main/java/br/com/samuelJacobsen/todolist/user/UserModel.java
+++ src/main/java/br/com/samuelJacobsen/todolist/user/UserModel.java
@@ -6,6 +6,7 @@ import java.util.UUID;
 import org.hibernate.annotations.CreationTimestamp;
 
 import jakarta.annotation.Generated;
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
@@ -19,7 +20,7 @@ public class UserModel {
     @GeneratedValue(generator = "UUID")
     private UUID id;
 
-   
+   @Column(unique = true)
     public String username;
     public String name;
     public String password;
